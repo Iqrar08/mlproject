b74d8bbb1aaf83a807caf23958067712a04ed62e
Iqrar08 <iqrara629@gmail.com>
2026-09-01 10:50:02 +0530

    docker file making

--- application.py
+++ application.py
@@ -9,9 +9,9 @@ application=Flask(__name__)
 
 app=application
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
 
 
 @app.route("/predictdata",methods=["GET","POST"])
@@ -37,4 +37,5 @@ def predict_datapoint():
     return render_template("home.html",results=results[0])
 
 if __name__=="__main__":
-    app.run(debug=True)
+    
+    app.run(host="0.0.0.0", port=5000)
